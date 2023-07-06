961a1d0f59956e237fdf1bee3fde2721ec4b6994
JakubTlak <marbixxd@gmail.com>
2023-07-06 11:03:20 +0200

    fetching css

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -2,7 +2,6 @@ import "./MainPage.css";
 import { useEffect, useState, useRef } from "react";
 import RecipeDetails from "./RecipeDetails";
 
-<<<<<<< HEAD
 
 function MainPage({
   recipeToShow,
@@ -11,9 +10,6 @@ function MainPage({
   logged,
   myIngredients
 }) {
-=======
-function MainPage({ recipeToShow, setRecipeToShow, setPage, logged }) {
->>>>>>> c70419e85962323530100cde4f31da396924393e
   const [searchInput, setSearchInput] = useState("");
   const [recipe, setRecipe] = useState([]);
   const [filteredMeals, setFilteredMeals] = useState([]);
@@ -131,10 +127,7 @@ function MainPage({ recipeToShow, setRecipeToShow, setPage, logged }) {
               ))}
             </div>
           )}
-<<<<<<< HEAD
           {showDetails && <RecipeDetails meal={recipeToShow} myIngredients={myIngredients}/>}
-=======
->>>>>>> c70419e85962323530100cde4f31da396924393e
         </div>
       </div>
       {showDetails && (
